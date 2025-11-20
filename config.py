06b33b02a72651f5a67d08f7babaa85322a4e93a
caminobelllo <alstpgml0812@gmail.com>
2025-11-20 17:30:32 +0900

    Chore: 2차 크롤링 완결 작품 url 추가

--- config.py
+++ config.py
@@ -18,6 +18,9 @@ GENRES = ["PURE", "FANTASY", "DAILY", "로판", "HISTORICAL"]
 # 2차 크롤링 URL (daily - 인기순)
 DAILY_PLUS_URL = 'https://comic.naver.com/webtoon?tab=dailyPlus'
 
+# 2차 크롤링 URL (completed - 완결작품)
+COMPLETED_URL = 'https://comic.naver.com/webtoon?tab=finish'
+
 GENRE_MAP = {
     "PURE": "로맨스",
     "FANTASY": "판타지",
